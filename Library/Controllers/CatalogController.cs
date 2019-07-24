5c6e3416d52b87cd5b6d9df35d79cca685ca2355
madehim <madehim333@yandex.ru>
2019-07-24 18:35:42 +0400

    Add project files.

---
+++ Library/Controllers/CatalogController.cs
@@ -0,0 +1,142 @@
+﻿using Library.Models.Catalog;
+using Library.Models.CheckoutModels;
+using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Library.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private ILibraryAsset _asset;
+        private ICheckout _checkouts;
+
+        public CatalogController(ILibraryAsset asset, ICheckout checkouts)
+        {
+            _asset = asset;
+            _checkouts = checkouts;
+        }
+
+        public IActionResult Index()
+        {
+            var assetModels = _asset.GetAll();
+
+            var listingResult = assetModels
+                .Select(result => new AssetIndexListingModel
+                {
+                    Id = result.Id,
+                    ImageUrl = result.ImageUrl,
+                    AuthorOrDirector = _asset.GetAuthorOrDirector(result.Id),
+                    DeweyCallNumber = _asset.GetDeweyIndex(result.Id),
+                    Title = result.Title,
+                    Type = _asset.GetType(result.Id)
+                });
+
+            var model = new AssetIndexModel()
+            {
+                Assets = listingResult
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var asset = _asset.GetById(id);
+
+            var currentHolds = _checkouts.GetCurrentHolds(id)
+                .Select(a => new AssetHoldModel
+                {
+                    HoldPlaced = _checkouts.GetCurrentHoldPlaced(a.Id),
+                    PatronName = _checkouts.GetCurrentHoldPatronName(a.Id)
+
+                });
+
+            var model = new AssetDetailModel
+            {
+                AssedId = id,
+                Title = asset.Title,
+                Type = _asset.GetType(id),
+                Year = asset.Year,
+                Cost = asset.Cost,
+                Status = asset.Status.Name,
+                ImageUrl = asset.ImageUrl,
+                AuthorOrDirector = _asset.GetAuthorOrDirector(id),
+                CurrentLocation = _asset.GetCurrentLocation(id).Name,
+                DeweyCallNumber = _asset.GetDeweyIndex(id),
+                CheckoutHistory = _checkouts.GetCheckoutHistory(id),
+                ISBN = _asset.GetIsbn(id),
+                LatestCheckout = _checkouts.GetLatestCheckout(id),
+                PatronName = _checkouts.GetCurrentCheckoutPatron(id),
+                CurrentHolds = currentHolds
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Checkout(int id)
+        {
+            var asset = _asset.GetById(id);
+
+            var model = new CheckoutModel
+            {
+                AssetId = id,
+                ImageUrl = asset.ImageUrl,
+                Title = asset.Title,
+                LibraryCardId = "",
+                IsCheckedOut = _checkouts.IsCheckedOut(id)
+            };
+
+            return View(model);
+        }
+
+        public IActionResult CheckIn(int id)
+        {
+            _checkouts.CheckInItem(id);
+            return RedirectToAction("Detail", new { id = id });
+        }
+
+        public IActionResult Hold(int id)
+        {
+            var asset = _asset.GetById(id);
+
+            var model = new CheckoutModel
+            {
+                AssetId = id,
+                ImageUrl = asset.ImageUrl,
+                Title = asset.Title,
+                LibraryCardId = "",
+                IsCheckedOut = _checkouts.IsCheckedOut(id),
+                HoldCount = _checkouts.GetCurrentHolds(id).Count()
+            };
+
+            return View(model);
+        }
+
+        public IActionResult MarkLost(int assetId)
+        {
+            _checkouts.MarkLost(assetId);
+            return RedirectToAction("Detail", new { id = assetId });
+        }
+
+        public IActionResult MarkFound(int assetId)
+        {
+            _checkouts.MarkFound(assetId);
+            return RedirectToAction("Detail", new { id = assetId });
+        }
+
+        [HttpPost]
+        public IActionResult PlaceCheckout(int assetId, int libraryCardId)
+        {
+            _checkouts.CheckOutItem(assetId, libraryCardId);
+            return RedirectToAction("Detail", new { id = assetId });
+        }
+
+        [HttpPost]
+        public IActionResult PlaceHold(int assetId, int libraryCardId)
+        {
+            _checkouts.CheckOutItem(assetId, libraryCardId);
+            return RedirectToAction("Detail", new { id = assetId });
+        }
+    }
+}
---
+++ LibraryData/Migrations/20190715145506_Fix naming.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryData.Migrations
+{
+    public partial class Fixnaming : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Adress",
+                table: "Patrons",
+                newName: "Address");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Patrons",
+                newName: "Adress");
+        }
+    }
+}
